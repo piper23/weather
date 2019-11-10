8ac6ca63e30e5d8092fc34c3949a603298b858f8
Reuben Gomes <reubengomes2@gmail.com>
2019-11-10 19:28:42 +0530

    Added get Location Mapped ForeCaste Updated UI

--- api.php
+++ api.php
@@ -5,48 +5,55 @@
 
 class API{
 
-private $apiKey="145645e544e640a0adb7aafa03d4e61d";
-public $apiURL =  "https://api.weatherbit.io/v2.0/forecast/daily?";
+	private $apiKey="145645e544e640a0adb7aafa03d4e61d";
+	public $apiURL =  "https://api.weatherbit.io/v2.0/forecast/daily?";
 
-public $date,$at,$long,$cityName = "";
-
-
-
-function getForeCast(){
-$str="city=".$this->city;
-if(isset($this->lat) && isset($this->long)){
-	$str="lat=".$this->lat."&lon=".$this->long;
-}
+	public $date,$lat,$long,$cityName = "";
 
 
-  $callingURL = $this->apiURL.$str."&key=".$this->apiKey;
 
-	$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,$callingURL);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	function getForeCast(){
+		$str="";
+		if(isset($this->city))
+			$str="city=".$this->city;
+		if(isset($this->lat) && isset($this->long)){
+			$str="lat=".$this->lat."&lon=".$this->long;
+		}
+
+
+		$callingURL = $this->apiURL.$str."&key=".$this->apiKey;
+
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$callingURL);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$output=curl_exec($ch);
-curl_close($ch);
-return json_decode( $output,true);
-}
+		$output=curl_exec($ch);
+		curl_close($ch);
+		return json_decode( $output,true);
+	}
 
 
 }
 
+$apiCall = new API();
 
 
-print_r(json_encode($_POST));
 
-//$m = new API();
+if(isset($_POST['coordinates'])){
+	$cord=json_decode($_POST['coordinates'],true);
 
-// $m->lat = "18.377986";
-// $m->long = "75.987924";
-// $m->city = "Mumbai";
-// echo("<pre>");
+	$apiCall->lat = $cord['lat'];
+	$apiCall->long =$cord['long'];
 
-// print_r($m->getForeCast());
-//curl_setopt($ch,CURLOPT_HEADER, true); //if you want headers
+}
+
+if(isset($_POST['city'])){
+	$apiCall->city = $_POST['city'];
+}
+echo json_encode($apiCall->getForeCast());
+
+
 ?>
--- index.php
+++ index.php
@@ -12,63 +12,56 @@
 </head>
 <body>
 	<header>
-		<h1>WEATHER APP</h1>
+		<h1 class=header-title>WEATHER APP</h1>
 	</header>
 	<article >
-		<section class="search-field">
-			 <div class="inputs-types">
-			<button type="" id="get-loc"><img src="assets/img/loc.png"></button>
-			</div>
-			 <div class="inputs-types mg-left">
-			 <input type="text" name="search" id="searchQuery" autocomplete="off" value="" placeholder="Enter City">
-			<button type="submit" class="search" id="search">Search</button>
-			 </div>
-			 <div class="inputs-types">
-			 	<label for="farcheck">
-			 		<input type="checkbox" name="Far" id="farcheck" value="F">	Farenheight
-			 	</label>
-			 	
-			 </div>
-		</section>
-		<section class="middle-section">
-			<div class="c-15">
-				<div id="date-time">
-					<div id="dayofweek"></div>
-					<div id="date">11-09-2019 : 7:30 PM</div>
-				</div>
-			</div>
-			<div class="c-70">
-				<div class="temp">
-					<h1 id="tempbind"></h1>
-				</div>
-				<div class="otherData">
-				<h3>High: <span id="highbind"></span> / Low: <span id="lowbind"></span></h3>
-				</div>
-			</div>
-			<div class="c-15">
-				<div id="location">
-					<h3 id="locatbind"></h3>
-				</div>
-			</div>	
-		</section>
-		<section class="forecast">
-			<div class="forecast-card">
-				<h4>Monday</h4>
-				<p class="temp">
-					24C	
-				</p>
-			</div>
-			<div class="forecast-card"></div>
-			<div class="forecast-card"></div>
-			<div class="forecast-card"></div>
-			<div class="forecast-card"></div>
-			<div class="forecast-card"></div>
-			<div class="forecast-card"></div>
-		</section>
+		<div class="container-flex">
+				<section class="search-field">
+						<div class="inputs-types">
+					   <button type="" id="get-loc" style="display: none"><img src="assets/img/loc.png"></button>
+					   </div>
+						<div class="inputs-types mg-left">
+						<input type="text" name="search" id="searchQuery" autocomplete="off" value="" placeholder="Enter City">
+					   <button type="submit" class="search" id="search">Search</button>
+						</div>
+						<div class="inputs-types">
+							<label for="farcheck">
+								<input type="checkbox" name="Far" id="farcheck" value="F">	Fahrenheit
+							</label>
+							
+						</div>
+				   </section>
+				   <section class="middle-section">
+					   <div class="c-15">
+						   <div id="date-time">
+							   <div id="dayofweek"></div>
+							   <div id="date"></div>
+						   </div>
+					   </div>
+					   <div class="c-70">
+						   <div class="temp">
+							   <h1 id="tempbind"></h1>
+						   </div>
+						   <div class="otherData">
+						   <h3>High: <span id="highbind"></span> / Low: <span id="lowbind"></span></h3>
+						   </div>
+					   </div>
+					   <div class="c-15">
+						   <div id="location">
+							   <h3 id="locatbind"></h3>
+						   </div>
+					   </div>	
+				   </section>
+				   <section class="forecast" id="forecast">
+			
+
+				   </section>
+		</div>
+
 	</article>
 <script type="text/javascript">
 var cityDefault = "Mumbai";
-
+var city="Mumbai";
 
 document.getElementById('searchQuery').addEventListener('keyup', (e)=>{
 	city = document.getElementById('searchQuery').value
@@ -76,7 +69,6 @@ document.getElementById('searchQuery').addEventListener('keyup', (e)=>{
 
 //Print Temp checks for Farenheight Toggle
 var printTemp = (temp)=>{
-	console.log(temp)
 return (c==true)?temp+"&#176;C":fConvert(temp)+"&#176;F"
 }
 function fConvert(temps){
@@ -104,13 +96,14 @@ return weekdays[dt.getDay()];
 }
 
 
-
+///Search
 
 document.getElementById("search").addEventListener("click", ()=>{
-if(city.length==""){
+if(city==""){
 	alert('City Field cannot be blank')
 	return false;	
 }
+ city=document.getElementById("searchQuery").value;
 
  fetchData(city);
 });
@@ -119,42 +112,85 @@ if(city.length==""){
 
 //Ajax Function
 var fetchData = (payload,def=1) =>{
+	
 var	passkey="city";
 	if(def==2)
 		passkey="coordinates";
 	var data = new FormData();
 data.append(passkey, JSON.stringify( payload) );
-
 fetch("api.php",
 {
     method: "POST",
     body: data
 })
 .then(function(res){ return res.json(); })
-.then(function(data){ console.log(data) })
+.then(function(data){ 
+	if(data == null){
+		alert("Weather not Found")
+		return false;
+	}
+	var wei = data.data
+	console.log(data)
+	state.todaysTemp = wei[0].temp
+	state.thigh = wei[0].high_temp
+	state.tlow = wei[0].low_temp
+	state.date= wei[0].valid_date
+	state.city= data.city_name
+	state.forecast = wei.slice(1,7)
+	console.log(state)
+	renderData();
+ })
 }
 
 
+// geo Functionality////////////
+if ("geolocation" in navigator) {
+var geoL = document.getElementById("get-loc")
+geoL.style.display = "block";
+geoL.addEventListener("click", ()=>{
+	navigator.geolocation.getCurrentPosition(function(position) {
+	let	coordinates = {'lat':position.coords.latitude,'long':position.coords.longitude}
+		fetchData(coordinates,2)
+
+});
+})
+
+
+}
 
 
 //Render Codes
-
-var city="";
 var c = true;
 
     const state = {
-      todaysTemp: '28',
-      thigh:"29",
-      tlow:"21",
+      todaysTemp: '',
+      thigh:"",
+      tlow:"",
       city: 'Mumbai',
-      date:"11-9-2019 20:15"
+      date:"",
+      forecast:[]
     };
 
 
+
+ var renderForecast = ()=>{
+var divAppend = document.getElementById("forecast")
+divAppend.innerHTML = "";
+	let futrueData = state.forecast;
+	futrueData.forEach(function(element) {
+	const div = document.createElement('div');
+	div.className = "forecast-card";
+	div.innerHTML = `<h4>${getDayofWeek(element.valid_date)}</h4><p class="temp">${printTemp(element.temp)}</p></div>`;
+	divAppend.appendChild(div)
+});
+
+ 		
+ }
 var renderTemp=()=>{
 document.getElementById("tempbind").innerHTML  =  printTemp(state.todaysTemp)
 document.getElementById("highbind").innerHTML  =  printTemp(state.thigh) 
-document.getElementById("lowbind").innerHTML  =  printTemp(state.tlow)  	
+document.getElementById("lowbind").innerHTML  =  printTemp(state.tlow) 
+ renderForecast() 	
 }
 
 
@@ -168,9 +204,9 @@ renderTemp();
 
 
 
- fetchData(state.city);
+ fetchData(city);
 
-renderData();
+
 </script>
 </body>
 </html>
